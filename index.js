54e607c23b274645a1b8783fa671b64f5a0e6d72
Ujjwal Sharma <usharma1998@gmail.com>
2018-02-26 15:25:21 +0530

    Use express to serve messages in JSON

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const irc = require("irc");
+const express = require("express");
 
+const app = express();
 const client = new irc.Client("chat.freenode.net", "kuken", {
   channels: ["#javascript", "#python", "#node.js", "#linux"],
 });
 
+let messages = [];
 client.addListener("raw", message => {
-  console.log(message);
+  messages.push(message);
 });
+
+app.get("/", (req, res) => res.json(messages));
+app.listen(3000, () => console.log("Listening on port 3000!"));
